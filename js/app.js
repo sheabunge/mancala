42082998cada35fe518f1a0ea556094638a908d3
Shea Bunge <info@bungeshea.com>
2015-08-11 21:22:03 +1000

    Revert "Change function and variable names to camelCase"
    This reverts commit cb52cbc87dfb83fc5966e2b4ce8a66febb841aca.

--- js/app.js
+++ js/app.js
@@ -6,70 +6,70 @@
 	window.mancala = {
 
 		player: 'one',
-		gameover: false,
+		game_over: false,
 
-		playerOnePits: document.querySelectorAll('.row.player-one .pit'),
-		playerTwoPits: document.querySelectorAll('.row.player-two .pit'),
+		player_one_pits: document.querySelectorAll('.row.player-one .pit'),
+		player_two_pits: document.querySelectorAll('.row.player-two .pit'),
 
-		playerOneStore: document.querySelector('.store.player-one'),
-		playerTwoStore: document.querySelector('.store.player-two'),
+		player_one_store: document.querySelector('.store.player-one'),
+		player_two_store: document.querySelector('.store.player-two'),
 
 		board: [],
 
 		/**
 		 * Build the board array with the current board configuration
 		 */
-		updateBoard: function () {
+		update_board: function () {
 			// current player store
-			this.board[6] = this.player === 'two' ? this.playerTwoStore : this.playerOneStore;
+			this.board[6] = this.player === 'two' ? this.player_two_store : this.player_one_store;
 
 			// other player store
-			this.board[13] = this.player === 'two' ? this.playerOneStore : this.playerTwoStore;
+			this.board[13] = this.player === 'two' ? this.player_one_store : this.player_two_store;
 
 			// current player pits
-			var currentPlayerPits = this.player === 'two' ? this.playerTwoPits : this.playerOnePits;
+			var current_player_pits = this.player === 'two' ? this.player_two_pits : this.player_one_pits;
 			for (var i = 0; i < 6; ++i) {
-				this.board[i] = currentPlayerPits[i];
+				this.board[i] = current_player_pits[i];
 			}
 
 			// other player pits
-			var otherPlayerPits = this.player === 'two' ? this.playerOnePits : this.playerTwoPits;
+			var other_player_pits = this.player === 'two' ? this.player_one_pits : this.player_two_pits;
 			for (i = 7; i < 13; ++i) {
-				this.board[i] = otherPlayerPits[i - 7];
+				this.board[i] = other_player_pits[i - 7];
 			}
 		},
 
-		getStones: function (pit) {
+		get_stones: function (pit) {
 			var stones = pit.querySelector('p').textContent;
 			return parseInt(stones);
 		},
 
-		setStones: function (pit, stones) {
+		set_stones: function (pit, stones) {
 			pit.querySelector('p').textContent = stones;
 		},
 
-		addStones: function (pit, stones) {
+		add_stones: function (pit, stones) {
 			var el = pit.querySelector('p');
 			el.textContent = parseInt(el.textContent) + stones;
 		},
 
-		doPlayerMove: function (starting_pit) {
+		do_user_move: function (starting_pit) {
 
 			// perform the player's action
-			var result = this.moveStones(starting_pit);
+			var result = this.move_stones(starting_pit);
 
 			// make sure that a player hasn't run out of stones
-			this.checkForWin();
+			this.check_for_win();
 
 			// change the player if the current turn is ended
-			if ( ! result && ! this.gameover ) {
-				this.switchTurn();
+			if ( ! result && ! this.game_over ) {
+				this.switch_turn();
 			}
 		},
 
-		switchTurn: function () {
+		switch_turn: function () {
 			this.player = this.player === 'one' ? 'two' : 'one';
-			this.updateBoard();
+			this.update_board();
 
 			var player = this.player;
 			setTimeout(function () {
@@ -78,17 +78,17 @@
 			}, 700 );
 		},
 
-		moveStones: function (starting_pit) {
-			this.updateBoard();
+		move_stones: function (starting_pit) {
+			this.update_board();
 
 			// return if pit has no stones
-			if (this.getStones(this.board[starting_pit]) < 1) {
+			if (this.get_stones(this.board[starting_pit]) < 1) {
 				return true;
 			}
 
 			// take stones out of pit
-			var stones = this.getStones(this.board[starting_pit]);
-			this.setStones(this.board[starting_pit], 0);
+			var stones = this.get_stones(this.board[starting_pit]);
+			this.set_stones(this.board[starting_pit], 0);
 
 			var pointer = starting_pit;
 			while (stones > 0) {
@@ -98,7 +98,7 @@
 					pointer = 0;
 				}
 
-				this.addStones(this.board[pointer], 1);
+				this.add_stones(this.board[pointer], 1);
 				stones--;
 			}
 
@@ -106,26 +106,26 @@
 			var inverse_pointer = 12 - pointer;
 
 			// Check for capture
-			if (pointer < 6 && this.getStones(this.board[pointer]) === 1 && this.getStones(this.board[inverse_pointer]) > 0) {
+			if (pointer < 6 && this.get_stones(this.board[pointer]) === 1 && this.get_stones(this.board[inverse_pointer]) > 0) {
 
 				// Transfer this stone along with opposite pit's stones to store
-				this.addStones(this.board[6], this.getStones(this.board[inverse_pointer]) + 1);
+				this.add_stones(this.board[6], this.get_stones(this.board[inverse_pointer]) + 1);
 
 				// Clear the pits
-				this.setStones(this.board[pointer], 0);
-				this.setStones(this.board[inverse_pointer], 0);
+				this.set_stones(this.board[pointer], 0);
+				this.set_stones(this.board[inverse_pointer], 0);
 			}
 
 			// return true if the turn ended in storage pit
 			return pointer === 6;
 		},
 
-		checkForWin: function () {
+		check_for_win: function () {
 			var that = this;
 
 			var is_row_empty = function (pits) {
 				for (var i = 0; i < pits.length; i++) {
-					if (that.getStones(pits[i]) > 0) {
+					if (that.get_stones(pits[i]) > 0) {
 						return false;
 					}
 				}
@@ -133,41 +133,41 @@
 				return true;
 			};
 
-			var player_one_out = is_row_empty(this.playerOnePits);
-			var player_two_out = is_row_empty(this.playerTwoPits);
+			var player_one_out = is_row_empty(this.player_one_pits);
+			var player_two_out = is_row_empty(this.player_two_pits);
 
 			// Take the stones from the non-empty row and add them to that player's store
 			if (! (player_one_out || player_two_out)) {
 				return;
 			}
 
-			this.gameover = true;
+			this.game_over = true;
 			document.body.classList.add('game-over');
 
-			var cleanUpRow = function (store, pits) {
-				var store_stones = that.getStones(store);
+			var cleanup_row = function (store, pits) {
+				var store_stones = that.get_stones(store);
 
 				pits.forEach(function (pit) {
-					store_stones += that.getStones(pit);
-					that.setStones(pit, 0);
+					store_stones += that.get_stones(pit);
+					that.set_stones(pit, 0);
 				});
 
-				that.setStones(store, store_stones);
+				that.set_stones(store, store_stones);
 			};
 
 			if (player_one_out && ! player_two_out) {
-				cleanUpRow(this.playerTwoStore, this.playerTwoPits);
+				cleanup_row(this.player_two_store, this.player_two_pits);
 
 			} else if (! player_one_out && player_two_out) {
-				cleanUpRow(this.playerOneStore, this.playerOnePits);
+				cleanup_row(this.player_one_store, this.player_one_pits);
 			}
 
 			var status = document.querySelector('.status');
 
 			// Determine which player holds the most stones
-			if (this.getStones(this.playerOneStore) > this.getStones(this.playerTwoStore)) {
+			if (this.get_stones(this.player_one_store) > this.get_stones(this.player_two_store)) {
 				status.textContent = 'Player one wins!';
-			} else if (this.getStones(this.playerTwoStore) > this.getStones(this.playerOneStore)) {
+			} else if (this.get_stones(this.player_two_store) > this.get_stones(this.player_one_store)) {
 				status.textContent = 'Player two wins!';
 			} else {
 				status.textContent = 'Draw!';
@@ -175,22 +175,22 @@
 		}
 	};
 
-	mancala.updateBoard();
+	mancala.update_board();
 
-	mancala.playerOnePits.forEach(function (pit, index) {
+	mancala.player_one_pits.forEach(function (pit, index) {
 		pit.setAttribute('data-pit', index);
 		pit.onclick = function () {
-			if (mancala.player === 'one' && ! mancala.gameover) {
-				mancala.doPlayerMove(this.getAttribute('data-pit'));
+			if (mancala.player === 'one' && ! mancala.game_over) {
+				mancala.do_user_move(this.getAttribute('data-pit'));
 			}
 		};
 	});
 
-	mancala.playerTwoPits.forEach(function (pit, index) {
+	mancala.player_two_pits.forEach(function (pit, index) {
 		pit.setAttribute('data-pit', index);
 		pit.onclick = function () {
-			if (mancala.player === 'two' && ! mancala.gameover) {
-				mancala.doPlayerMove(this.getAttribute('data-pit'));
+			if (mancala.player === 'two' && ! mancala.game_over) {
+				mancala.do_user_move(this.getAttribute('data-pit'));
 			}
 		};
 	});
